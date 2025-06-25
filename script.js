aab74f8645487dfcbaf02ad6861d623175e9940c
Amaki1998 <105007806+Amaki1998@users.noreply.github.com>
2025-06-26 00:20:52 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+// Basic form submission alert (for demo)
+document.querySelector('form').addEventListener('submit', (e) => {
+    e.preventDefault();
+    alert('Message sent! (This is a demo.)');
+});
